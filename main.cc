b936ffe7a6bcb81ab38908af8f530155b8b51521
nk_hard <ivanternovyi@gmail.com>
2017-03-08 15:49:45 +0200

    Update main.cc

--- main.cc
+++ main.cc
@@ -7,10 +7,10 @@ int main()
 {
 	task t;
 	t.set_task ();
-	t.show_status ();
-	t.reduce_task ();
+	//t.show_status ();
+	//t.reduce_task ();
 	t.upgrade_task ();
-	t.upgrade_task ();
+	//t.upgrade_task ();
 	t.show_task ();
 
 	return 0;
